3d50f3c23aab09885ca4a0e4560e3e25e314b254
dannycahyo <dannydwicahyono@gmail.com>
2023-10-29 20:43:24 +0700

    feat: add promises challenge

---
+++ javascript/promises/index.js
@@ -0,0 +1,92 @@
+const STATE = {
+  PENDING: "pending",
+  FULFILLED: "fulfilled",
+  REJECTED: "rejected",
+};
+
+class MyPromise {
+  #state = STATE.PENDING;
+  #value = null;
+  #fullfilledCallbacks = [];
+  #rejectedCallbacks = [];
+
+  constructor(executorFunc) {
+    try {
+      executorFunc(
+        (value) => this.#resolve(value),
+        (value) => this.#reject(value),
+      );
+    } catch (error) {
+      this.#reject(error);
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    return new MyPromise((resolve, reject) => {
+      const fulfilledCallback = () => {
+        if (!onFulfilled) return resolve(this.#value);
+
+        queueMicrotask(() => {
+          try {
+            const value = onFulfilled(this.#value);
+            resolve(value);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      };
+
+      const rejectedCallback = () => {
+        if (!onRejected) return reject(this.#value);
+
+        queueMicrotask(() => {
+          try {
+            const value = onRejected(this.#value);
+            resolve(value);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      };
+
+      switch (this.#state) {
+        case STATE.PENDING:
+          this.#fullfilledCallbacks.push(fulfilledCallback);
+          this.#rejectedCallbacks.push(rejectedCallback);
+          break;
+        case STATE.FULFILLED:
+          fulfilledCallback();
+          break;
+        case STATE.REJECTED:
+          rejectedCallback();
+          break;
+        default:
+          throw new Error("Unexpected promise state");
+      }
+    });
+  }
+
+  catch(onRejected) {
+    return this.then(null, onRejected);
+  }
+
+  get state() {
+    return this.#state;
+  }
+
+  get value() {
+    return this.#value;
+  }
+
+  #resolve(value) {
+    this.#value = value;
+    this.#state = STATE.FULFILLED;
+    this.#fullfilledCallbacks.forEach((callback) => callback());
+  }
+
+  #reject(value) {
+    this.#value = value;
+    this.#state = STATE.REJECTED;
+    this.#rejectedCallbacks.forEach((callback) => callback());
+  }
+}
