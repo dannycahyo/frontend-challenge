17032968fa77483ca6f4d8d379c0af37d2ef53fe
dannydcahyono <dannydwicahyono@gmail.com>
2023-11-01 20:58:30 +0700

    feat: add promisify challenge

---
+++ javascript/promisify/index.js
@@ -0,0 +1,15 @@
+function promisify(callback) {
+  return function (...args) {
+    return new Promise((resolve, reject) => {
+      function handleErrorAndValue(error, value) {
+        if (error == null) {
+          resolve(value);
+        } else {
+          reject(error);
+        }
+      }
+
+      callback.call(this, ...args, handleErrorAndValue);
+    });
+  };
+}
