8688e73f37f55ec5cdfcc7ae0397280872fffedd
dannydcahyono <dannydwicahyono@gmail.com>
2023-11-03 08:34:57 +0700

    feat: add this binding challenge

---
+++ javascript/this-binding/index.js
@@ -0,0 +1,16 @@
+Function.prototype.myCall = function (thisContext, ...args) {
+  const symbol = Symbol();
+  thisContext[symbol] = this;
+  const result = thisContext[symbol](...args);
+  delete thisContext[symbol];
+
+  return result;
+};
+
+Function.prototype.myApply = function (thisContext, args = []) {
+  return this.myCall(thisContext, ...args);
+};
+
+Function.prototype.myBind = function (thisContext, ...args) {
+  return (...newArgs) => this.myApply(thisContext, [...args, ...newArgs]);
+};
